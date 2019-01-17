21a5b5bb6f73c41e13015ac40c2beffbc7506b2f
Luki R Daniel <luki.rompis@cicil.co.id>
2019-01-17 18:31:00 +0700

    add authorization using global variable

--- Object Repository/REST_Cicil/Hit Email Verification API - Staging Main.rs
+++ Object Repository/REST_Cicil/Hit Email Verification API - Staging Main.rs
@@ -9,13 +9,41 @@
    <httpBody></httpBody>
    <httpBodyContent></httpBodyContent>
    <httpBodyType></httpBodyType>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Authorization</name>
+      <type>Main</type>
+      <value>${token}</value>
+   </httpHeaderProperties>
    <migratedVersion>5.4.1</migratedVersion>
    <restRequestMethod>GET</restRequestMethod>
-   <restUrl>http://api-staging.cicil.co.id/v1/registration/activate_email</restUrl>
+   <restUrl>http://api-staging.cicil.co.id/v1/registration/activate_email?</restUrl>
    <serviceType>RESTful</serviceType>
    <soapBody></soapBody>
    <soapHeader></soapHeader>
    <soapRequestMethod></soapRequestMethod>
    <soapServiceFunction></soapServiceFunction>
+   <variables>
+      <defaultValue>GlobalVariable.authToken</defaultValue>
+      <description></description>
+      <id>7b29ba44-029d-43b8-89a3-2af6426c9e7e</id>
+      <masked>false</masked>
+      <name>token</name>
+   </variables>
+   <verificationScript>import static org.assertj.core.api.Assertions.*
+
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webservice.verification.WSResponseManager
+
+import groovy.json.JsonSlurper
+import internal.GlobalVariable as GlobalVariable
+
+RequestObject request = WSResponseManager.getInstance().getCurrentRequest()
+
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()
+</verificationScript>
    <wsdlAddress></wsdlAddress>
 </WebServiceRequestEntity>
--- Scripts/CASE_Cicil/stagingbeta-verifyemail/Script1547548418276.groovy
+++ Scripts/CASE_Cicil/stagingbeta-verifyemail/Script1547548418276.groovy
@@ -1,48 +1,55 @@
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
 
-import org.junit.After
+import org.openqa.selenium.Cookie
+import org.openqa.selenium.WebDriver
 
-import com.google.gson.JsonElement
+import com.kms.katalon.core.exception.StepFailedException
+import com.kms.katalon.core.testobject.ConditionType
 import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.TestObjectProperty
 import com.kms.katalon.core.testobject.impl.HttpTextBodyContent
+import com.kms.katalon.core.util.KeywordUtil
 import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.driver.DriverFactory
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+
 import groovy.json.JsonSlurper
+import internal.GlobalVariable
 
-WebUI.openBrowser('staging-beta.cicil.co.id')
-
-WebUI.maximizeWindow()
-
-// tutup pop up
-WebUI.waitForElementPresent(findTestObject('Page_CICIL_login/div_tutup'), 5)
-
-WebUI.waitForElementVisible(findTestObject('Page_CICIL_login/div_tutup'), 5)
-
-//WebUI.waitForElementPresent(findTestObject('Page_CICIL_login/span_tutup'), 5)
-WebUI.click(findTestObject('Page_CICIL_login/div_tutup'))
-
-Thread.sleep(1500)
-
-WebUI.verifyElementNotVisible(findTestObject('Page_CICIL_login/div_tutup'))
-
-//WebUI.verifyElementNotPresent(findTestObject('Page_CICIL_login/span_tutup'), 1)
-// klik masuk
-WebUI.click(findTestObject('Page_CICIL_login/a_Masuk'))
-
-// input uname, pass, press masuk
-WebUI.waitForElementPresent(findTestObject('Page_CICIL_login/input_username'), 10)
-WebUI.setText(findTestObject('Page_CICIL_login/input_username'), 'testing+katalonia5@cicil.co.id')
-WebUI.setText(findTestObject('Page_CICIL_login/input_password'), '1234567A')
-WebUI.click(findTestObject('Page_CICIL_login/button_Masuk'))
-
-// should be landed on email verification page
-Thread.sleep(3000)
-WebUI.waitForElementPresent(findTestObject('Page_CICIL_login/h4_Verifikasi Email'), 10)
-println '>>> landed on verification link page'
+//WebUI.openBrowser('staging-beta.cicil.co.id')
+//
+//WebUI.maximizeWindow()
+//
+//// tutup pop up
+//WebUI.waitForElementPresent(findTestObject('Page_CICIL_login/div_tutup'), 5)
+//
+//WebUI.waitForElementVisible(findTestObject('Page_CICIL_login/div_tutup'), 5)
+//
+////WebUI.waitForElementPresent(findTestObject('Page_CICIL_login/span_tutup'), 5)
+//WebUI.click(findTestObject('Page_CICIL_login/div_tutup'))
+//
+//Thread.sleep(1500)
+//
+//WebUI.verifyElementNotVisible(findTestObject('Page_CICIL_login/div_tutup'))
+//
+////WebUI.verifyElementNotPresent(findTestObject('Page_CICIL_login/span_tutup'), 1)
+//// klik masuk
+//WebUI.click(findTestObject('Page_CICIL_login/a_Masuk'))
+//
+//// input uname, pass, press masuk
+//WebUI.waitForElementPresent(findTestObject('Page_CICIL_login/input_username'), 10)
+//WebUI.setText(findTestObject('Page_CICIL_login/input_username'), 'testing+lukiverif5@cicil.co.id')
+//WebUI.setText(findTestObject('Page_CICIL_login/input_password'), '1234567A')
+//WebUI.click(findTestObject('Page_CICIL_login/button_Masuk'))
+//
+//// should be landed on email verification page
+//Thread.sleep(3000)
+//WebUI.waitForElementPresent(findTestObject('Page_CICIL_login/h4_Verifikasi Email'), 10)
+//println '>>> landed on verification link page'
 
 // setting up login request (https://stackoverflow.com/questions/50049056/how-to-use-setbodycontenthttpbodycontent-in-katalon)
 RequestObject loginRequest = findTestObject('Object Repository/REST_Cicil/Hit Login API - Staging Main')
-String requestBody = '{   "username": "testing+katalonia5@cicil.co.id",   "password": "1234567A" }'
+String requestBody = '{   "username": "testing+lukiverif5@cicil.co.id",   "password": "1234567A" }'
 loginRequest.setBodyContent(new HttpTextBodyContent(requestBody))
 
 // hit login 
@@ -58,14 +65,36 @@ JsonSlurper slurper = new JsonSlurper()
 Map responseParsed = slurper.parseText(responseString)
 println '>>> token: ' + responseParsed.token
 
-//
+// manually create the request
+RequestObject requestActivate = new RequestObject('activator')
+//requestActivate.setBodyContent(null)
+//TestObjectProperty propCookie = new TestObjectProperty("token", ConditionType.EQUALS, responseParsed.token)
+//TestObjectProperty propPath = new TestObjectProperty("path", ConditionType.EQUALS, '/')
+//TestObjectProperty propDomain = new TestObjectProperty("domain", ConditionType.EQUALS, '.api-staging.cicil.co.id')
+TestObjectProperty propToken = new TestObjectProperty('Authorization', ConditionType.EQUALS, responseParsed.token.toString())
+ArrayList headers = Arrays.asList(propToken)
+
+//requestActivate.setHttpHeaderProperties(headers)
+GlobalVariable.authToken = responseParsed.token
+requestActivate.setRestUrl('http://api-staging.cicil.co.id/v1/registration/activate_email')
+requestActivate.setRestRequestMethod('GET')
+
 //// set login cookie (https://docs.katalon.com/katalon-studio/docs/set-cookies-for-browsers.html)
+//println '>>> setting login cookie'
+//Cookie ckToken = new Cookie("token", responseParsed.token);
 //WebDriver webDriver = DriverFactory.getWebDriver();
-//webDriver.manage().addCookie(null);
+//webDriver.manage().addCookie(ckToken)
 //
-//// hit the activation link
-//println '>>> hitting activation API'
-//verifResponse = WS.sendRequest(findTestObject('Object Repository/REST_Cicil/Hit Email Verification API - Staging Main'))
-//WS.verifyResponseStatusCode(verifResponse, 200)
-//
-//
+// hit the activation link
+println '>>> hitting activation API'
+verifResponse = WS.sendRequest(findTestObject('Object Repository/REST_Cicil/Hit Email Verification API - Staging Main'))
+try{
+	WS.verifyResponseStatusCode(verifResponse, 200)
+	println '>>> activation response: ' + verifResponse.getResponseBodyContent()
+	// close
+	WebUI.closeBrowser()
+} catch(StepFailedException se){
+	println '>>> failed. closing browser'
+	println '>>> activation response: ' + verifResponse.getResponseBodyContent()
+	WebUI.closeBrowser()
+}
